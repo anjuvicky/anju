7a116afa19d22c215eaa2d0c001c270286ed93ae
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-03-06 17:48:24 +0530

    Create ov.c

---
+++ ov.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    
+       printf("Enter any number: ");
+    scanf("%d", &n);
+    
+
+    if(num > 0)
+    {
+        printf("Number is POSITIVE");
+    }
+    if(num < 0)
+    {
+        printf("Number is NEGATIVE");
+    }
+    if(num == 0)
+    {
+        printf("Number is ZERO");
+    }
+
+    return 0;
+}
