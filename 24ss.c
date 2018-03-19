5af7b85167d4155c6c75ebb4168533eeb4a686cf
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-03-19 17:44:58 +0530

    Create 24ss.c

---
+++ 24ss.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+void main()
+{
+  int array[10000], maximum, size, c, location = 1;
+ 
+  printf("Enter the number of elements in array\n");
+  scanf("%d", &size);
+ 
+
+ 
+  for (c = 0; c < size; c++)
+{
+median array[10000];
+    
+ 
+
+  for (c = 1; c < size; c++)
+  {
+    if (array[c] > maximum)
+    {
+       median  = array[c];
+       location = c+1;
+    }
+  }
+ 
