fad8265421a4295520988df52582a056cd18f3ab
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-03-19 17:47:49 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,15 @@
+import java.util;
+
+public static boolean isNumeric(String str)  
+{  
+  try  
+  {  
+    double d = Double.parseDouble(str);  
+  }  
+  catch(NumberFormatException nfe)  
+  {  
+    return false;  
+  }  
+  return true;  
+}
+ }
