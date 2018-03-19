302ff19165aaf4032685a2673621e0e2004bd1b1
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-03-19 17:42:45 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+void main()
+{
+  int array[100], maximum, size, c, location = 1;
+ 
+  printf("Enter the number of elements in array\n");
+  scanf("%d", &size);
+ 
+
+ 
+  for (c = 0; c < size; c++)
+{
+median array[100];
+    
+ 
+
+  for (c = 1; c < size; c++)
+  {
+    if (array[c] > maximum)
+    {
+       median  = array[c];
+       location = c+1;
+    }
+  }
+ 
