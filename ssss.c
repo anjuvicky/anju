2ce6e12647fafde795712782f6fd0b10f25a34bf
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-03-19 17:39:19 +0530

    Create ssss.c

---
+++ ssss.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+void main()
+{
+  int array[100], maximum, size, c, location = 1;
+ 
+  printf("Enter the number of elements in array\n");
+  scanf("%d", &size);
+ 
+
+ 
+  for (c = 0; c < size; c++)
+{
+sort array[100];
+    
+ 
+
+  for (c = 1; c < size; c++)
+  {
+    if (array[c] > maximum)
+    {
+       maximum  = array[c];
+       location = c+1;
+    }
+  }
+ 
