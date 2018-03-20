0bc9027d8b675294a85f6b693ad5a24d6bb3c131
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-03-20 17:13:46 +0530

    Update 337.c

--- 337.c
+++ 337.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
   #include <string.h>
 
-  int main() {
+void main() {
         char string[100], even[100], odd[100], i, j, k;
 
         i = j = k = 0;
@@ -14,11 +14,7 @@
      
         printf("Given Input string:%s\n", string);
 
-        /*
-         * print the characters at odd position in odd array.
-         * Similarly, print the characters at even position in
-         * even array.
-         */
+       
         while (string[i] != '\0') {
                 if (i % 2 == 0) {
                         odd[j++] = string[i];
@@ -28,11 +24,11 @@
                 i++;
         }
 
-        /* terminating even and odd string with NULL */
+        
         odd[j] = even[k] = '\0';
 
-        /* print the charactersa at odd position and even positions */
+      
         printf("Characters at odd position: %s\n", odd);
         printf("Characters at even position: %s\n", even);
-        return 0;
+       
   }
