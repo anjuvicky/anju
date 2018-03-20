48ff75228eac35df703bc0dd0b99dd958e1a2d81
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-03-20 17:29:01 +0530

    Create p9.c

---
+++ p9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ 
+nt i,j,flag,count=0; 
+for(i=0 ;i<10;i++) 
+{ 
+flag=0; 
+for(j=2;j<=a[i]/2;j++) 
+{ 
+if (a[i]%j==0) 
+{ 
+flag=1; 
+break; 
+} 
+} 
+if (flag==0) 
+count++; 
+} 
