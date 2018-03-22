400249f6c23e62d87a38b4a4890d610622779b85
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-03-22 17:08:48 +0530

    Create pl5.py

---
+++ pl5.py
@@ -0,0 +1,16 @@
+str = "Computers are useless. "  
+  
+
+dic = {}  
+for char in str:  
+   dic[char] = 0  
+  
+
+for char in str:  
+  
+   dic[char]+= 1  
+  
+  
+for char, count in dic.items():  
+   if count > 1 and char != ' ':  
+      print "%c is repeated %d times" % (char, count)  
