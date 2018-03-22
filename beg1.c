4570ec0bb78afba59ef812dc29a5872bd0ee2d8f
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-03-22 17:36:52 +0530

    Create beg1.c

---
+++ beg1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int m;
+const char * z = "testing one two three";
+int charcount;
+
+charcount = 0;
+for(m=0; z[m]; m++) {
+    if(z[m] != ' ') {
+        charcount ++;
+    }
+}}
