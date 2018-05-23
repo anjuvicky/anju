d2b745ccddb6b3b28a4dc7cbcddc1bddc32981f1
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-05-23 10:40:03 +0530

    Create 66.cpp

---
+++ 66.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+int main()
+{
+int a,b,c;
+cout<<"enter the firstnum";
+cin>>a;
+cout<<"enter the second number";
+cin>>b;
+c=a*b;
+cout<<"product is"<<c;
+if(c%2==0)
+{
+cout<<"even";
+}
+else
+{
+cout<<"odd";
+}
+return 0;
+}
