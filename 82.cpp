0663523f2318011759775d58b3dd79764fde99c7
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-05-23 14:31:27 +0530

    Create 82.cpp

---
+++ 82.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+char str[20];
+cout<<"enter the string";
+cin>>str;
+if(str=='a' || str=='e'|| str=='i' || str=='o' || str=='u')
+{
+cout<<"yes";
+}
+else
+{
+cout<<"no";
+}
+return 0;
+}
