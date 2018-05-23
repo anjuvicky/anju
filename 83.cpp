61aac9a6d97640aa18c238d06884879130f12983
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-05-23 14:43:54 +0530

    Create 83.cpp

---
+++ 83.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,l,r;
+cout<<"enter the number";
+cin>>n;
+cout<<"enter the num";
+cin>>l;
+cout<<"enter the num2";
+cin>>r;
+if(n<=l && n>=r)
+{
+cout<<"yes";
+}
+else
+{
+cout<<"no";
+}
+return 0;
+}
