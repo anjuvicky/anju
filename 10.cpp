93fb50991604afeedf0b7e81a0950a05474a8eb2
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-05-23 09:33:47 +0530

    Create 10.cpp

---
+++ 10.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+voidmain()
+{
+int largestNumber;
+
+int numbers[n];
+
+largestNumber=numbers[0];
+
+for (i=0 ; i<n; i++)
+{
+if (numbers[i] > largestNumber) 
+{
+largest = numbers[i];
+}}
