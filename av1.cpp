3b8a9c734b878d21be26793da98139bb90082536
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-05-24 15:35:10 +0530

    Create av1.cpp

---
+++ av1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int i,n,j;
+  char a[n];
+ 
+  cout<<"enter the size of string";
+  cin>>n;
+  cout<<"enter the string";
+  cin>>a;
+  cout<<"enter the size";
+  cin>>i;
+  for(j=0;j<=a[n];j++)
+  {
+      cout<<a[n];
+  }
+  return 0;
+}
+  
+  
