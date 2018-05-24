87aaec90124fa73d40648a22ff7f35f5ab9e4071
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-05-24 10:11:01 +0530

    Create 91.cpp

---
+++ 91.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a1,a2,a3,b1,b2,b3,c1,c2,c3;
+    cout<<"enter kabali ninja num1";
+    cin>>a1;
+    cout<<"enter opp ninja num1";
+    cin>>a2;
+    cout<<"enter kabali ninja num2";
+    cin>>b1;
+    cout<<"enter opp ninja num2";
+    cin>>b2;
+    cout<<"enter kabali ninja num3";
+    cin>>c1;
+    cout<<"enter opp ninja num3";
+    cin>>c2;
+    a3=a1+a2;
+    cout<<a3<<endl;
+    b3=b1+b2;
+    cout<<b3<<endl;
+    c3=c1+c2;
+    cout<<c3<<endl;
+    return 0;
+}
