90b1f6fbfa5ff1df01fd554a373e4b3c75fff19e
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-05-24 16:05:38 +0530

    Create av6.cpp

---
+++ av6.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n,op;
+ cout<<"enter the number";
+ cin>>n;
+ cout<<"enetr op"<<op;
+ switch(n)
+ {
+     case 1:
+     cout<<"a";
+     break;
+     case 2:
+     cout<<"b";
+     break;
+     case 3:
+     cout<<"c";
+     break;
+     case 4:
+     cout<<"5";
+     break;
+     case 6:
+     cout<<"6";
+     break;
+     case 7:
+     cout<<"7";
+     break;
+     case 8:
+     cout<<"8";
+     break;
+ }
+ return 0;
+ }
+ 
