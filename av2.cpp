b4dcef9a22021417f17f42179539befb5e1433d9
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-05-24 15:44:18 +0530

    Create av2.cpp

---
+++ av2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  cout<<"enter the number";
+  cin>>a;
+  if(a/2==0)
+  {
+      b=a/2;
+      cout<<b;
+  }
+      else
+      {
+      
+          cout<<a;
+      
+  }
+  return 0;
+}
