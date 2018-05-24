53b0d1e5ccbc38737dda2d1eccf8822d77eba42f
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-05-24 10:35:03 +0530

    Create 92.cpp

---
+++ 92.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float len,bre,area;
+    cout<<"enter the length";
+    cin>>len;
+    cout<<"enter the bredth";
+    cin>>bre;
+    area=len*bre;
+    cout<<"enter area"<<area;
+    return 0;
+}
