6cc09e9163adf46d182a181403a7a1c52934adf9
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-05-24 14:31:53 +0530

    Create 666v.cpp

---
+++ 666v.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,i,count=0;
+cout<<"enter the number";
+cin>>n;
+for(i=2;i<=n;i++)
+{
+if(n%i==0)
+{
+count++;
+}
+if(count==0)
+{
+cout<<"prime";
+}
+else
+{
+cout<<"no";
+}
+}
+return 0;
+}
