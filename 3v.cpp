da2973dd7e97bd711ad962cc40ddd786db5d5fa7
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-05-24 14:06:31 +0530

    Create 3v.cpp

---
+++ 3v.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int cel,kel;
+   cout<<"temperature in celcius";
+   cin>>cel;
+   kel=cel+273.15;
+   cout<<"conversion of kelvin is:"<<kel;
+   return 0;
+}
