bea6c0668f95761698c1e8a693c49cc1f1760fb4
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-05-25 10:06:57 +0530

    Create 05.cpp

---
+++ 05.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,k;
+    
+    cin<<n<<k;
+    char m[3];
+    cout<<"enter the numbers";
+    cin>>m;
+    if(m[0]<m[1)
+    {
+        if(m[1]<m[2)
+        {
+            cout<<m[1];
+        }
+        else
+        {
+            cout<<m[2];
+        }
+        
+    }
+    return 0;
+}
