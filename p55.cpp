5b6a96fb1ddaa0324d36188568a647bfc35e005b
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-05-25 12:04:52 +0530

    Create p55.cpp

---
+++ p55.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a[];
+int n;
+cout<<"size of array";
+cin>>n;
+cout<<"enter the array";
+cin>>a;
+cout<<"aftr sorting"<<sort(a);
+return 0;
+}
