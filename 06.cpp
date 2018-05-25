64f356fdbed79b52d2bb8826ea7dedb00756dae8
anjuvicky <37071749+anjuvicky@users.noreply.github.com>
2018-05-25 10:14:07 +0530

    Create 06.cpp

---
+++ 06.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string  s1;
+    string s2;
+     string  s3;
+     cin>>s1>>s2;
+     s3=s1+s2;
+     cout<<s3;
+     
+}
